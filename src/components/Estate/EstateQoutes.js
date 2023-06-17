c81090aed012e5141770e46227c46560aba6e23a
vicson <vicsonabuka@gmail.com>
2023-06-17 19:27:18 +0100

    Added created the header component and the EstateQoute component

--- src/components/Estate/EstateQoutes.js
+++ src/components/Estate/EstateQoutes.js
@@ -1,18 +1,36 @@
-import React from 'react'
+import {useState} from 'react'
 import styles from './EstateQoutes.module.css';
 import {dummyQoutes} from './QouteIndex';
 
-function EstateQoutes(props) {
-    let index = 0;
+function EstateQoutes() {
+    let [index, setIndex] = useState(0);
     const post = dummyQoutes[index];
+
+    const firstPost = () => {
+      setIndex(index = 0);
+    }
+    const seconPost = () => {
+      setIndex(index = 1);
+    }
+    const thirdPost = () => {
+      setIndex(index = 2);
+    }
   return (
     
     <div className={styles.qoutes}>
         <div className={styles.post}>
-            <h1>{post.title}</h1>
+            <h2>{post.title}</h2>
             <p>{post.description}</p>
         </div>
-        <img src='/images/wallpaper.jpg'/>
+        
+     <div className={styles.slider}>
+        <input type='radio' name='default' onClick={firstPost} value='first' defaultChecked/>
+        <input type='radio' name='default' onClick={seconPost} value='second'/>
+        <input type='radio' name='default' onClick={thirdPost} value='third'/>
+       
+      
+      </div>
+     
     </div>
 
   )
